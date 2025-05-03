5683a1d3884a97bbbd517b2dc6991658102c359c
11476 <gjw3503@gmail.com>
2025-05-03 10:39:02 -0400

    basic interface idk

--- main.cpp
+++ main.cpp
@@ -7,28 +7,58 @@
 
 #include <iostream>
 #include <bitset>
-#define pos 7*r+c
-#define red 0
-#define yellow 1
+
 //7c 6r
 //phenylalanine
-typedef struct twice_bitset_42{
+const bool red = false, yellow = true;
+inline int P(int a, int b)
+{
+    return 7 * a + b;
+}
+class Board
+{
     std::bitset<42> filled;
     std::bitset<42> colors;
-    inline bool is_red(int r, int c){
-        return (filled[pos] and colors[pos] == red);
-    }
-    inline bool is_yellow(int r, int c){
-        return (filled[pos] and colors[pos] == yellow);
-    }
-    inline bool is_empty(int r, int c){
-        return not filled[pos];
-    }
+    public:
     inline int get_state(int r, int c){
-        if (not filled[pos]) return 0;
-        return 1+colors[pos];
+        if (not filled[P(r, c)]) return 0;
+        return 1+colors[P(r, c)];
     }
-} board;
-int main(int argc, const char * argv[]) {
+    Board(){
+        filled = std::bitset<42>(0);
+        colors = std::bitset<42>(0);
+    }
+    void move (int c, bool player){
+        int r1 = P(0, c);
+        while (filled[r1]){
+            r1 += 7;
+        }
+        filled[r1] = true;
+        colors[r1] = player;
+    }
+};
 
+void display(Board& board){
+    using std::cout;
+    using std::endl;
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            int state = board.get_state(i, j);
+            if (state == 0) cout << ".";
+            else if (state == 1) cout << "R";
+            else if (state == 2) cout << "Y";
+            else cout << "?";
+        }
+        cout << endl;
+    }
+}
+int main(int argc, const char *argv[])
+{
+    // driver code
+    Board board;
+    display(board);
+    board.move(3, red);
+    display(board);
 }
